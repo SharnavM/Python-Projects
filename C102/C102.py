b5e26ec40810b5686bae4fe67a8eacaed1555015
TechySharnav <sharnav123@gmail.com>
2021-02-24 19:43:43 +0530

    C102

--- C102/C102.py
+++ C102/C102.py
@@ -1,19 +1,33 @@
 import cv2
 import dropbox
-import os
+import time
+from datetime import datetime
 def snap():
-    vid_cap = cv2.VideoCapture(0)
-    flag = True
-    os.chdir("A:\WhiteHatJr\Python\C102")
-    while flag:
-       boolean, frame = vid_cap.read()
-       print("frane ", frame)
-       print(boolean)
-       cv2.imwrite("./frame.jpg",frame)
-       flag = False
+    vid_cap =  cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    #flag = True
+    boolean, frame = vid_cap.read()
+    return frame
+    #flag = False
 
     vid_cap.release()
-    cv2.destroyAllWindows()
     print("Frame Captured")
 
-snap()
+def upload(access_token,f,dt):
+    name = f
+    dbx = dropbox.Dropbox(access_token)
+    f = open(f, "rb")
+    dbx.files_upload(f.read(), f"/Security/{dt}/{name}")
+
+count = 0
+
+
+while True:
+    now = datetime.now()
+    f = snap()
+    dt = now.date()
+    ti = now.strftime("%H-%M-%S")
+    cv2.imwrite(f"frame_{ti}.jpg",f)
+    print(f"frame_{ti}.jpg")
+    upload("1WY4KQtQnM4AAAAAAAAAAWJ0qRraCzP5CXWJoqzLcudTnxnIALMPGcqsJriEmoX-",f"frame_{ti}.jpg", dt)
+    count += 1
+    time.sleep(10)
